6cd44c4bf6f9e6c04d7b0e86e7942eff6a120f10
Akande Daniel <akandedaniel86@gmial.com>
2023-08-06 14:25:19 +0100

    commit

---
+++ assets/js/preloader.js
@@ -0,0 +1,12 @@
+// preloader - start
+  // --------------------------------------------------
+ // Wait for the page to load
+ // Wait for the page to load
+ window.addEventListener('load', function () {
+  var preloader = document.getElementById('preloader');
+  preloader.style.display = 'none'; // Hide the preloader once the page has fully loaded
+});
+  
+    // preloader - end
+    // --------------------------------------------------
+  
